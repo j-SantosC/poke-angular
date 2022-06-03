17d6b1ea9901dd40f08004f1425220080139748c
Jacobo Santos Cores <jsantoscores@gmail.com>
2022-06-03 19:36:18 +0200

    Creating Repo
    First commit in poke-project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,4 +7,9 @@ const routes: Routes = [];
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
+const routes: Routes = [
+  { path: '', component: HomeComponent }
+];
+
 export class AppRoutingModule { }
