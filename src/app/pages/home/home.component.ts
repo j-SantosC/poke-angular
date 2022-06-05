d9792c3bf1bb8ab2a0d8099c769a118c37302fd7
Jacobo Santos Cores <jsantoscores@gmail.com>
2022-06-05 17:15:13 +0200

    Changed Pikachu as initial pokemon
    Test for cherry pick
    
    # Conflicts:
    #	src/app/pages/home/home.component.ts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -16,7 +16,7 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.pushInitialPoke('bulbasaur');
     this.pushInitialPoke('charmander');
-    this.pushInitialPoke('vulpix');
+    this.pushInitialPoke('pikachu');
     console.log(this.initialPokemon);
   }
   pushInitialPoke(poke: string) {
