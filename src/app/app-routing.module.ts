06405e242d6aa98dfff1859a2b25f260c5e03a14
Jacobo Santos <jsantoscores@gmail.com>
2022-11-12 19:47:55 +0100

    remove crap and leave main game

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 import { SearchComponent } from './pages/search/search.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent, pathMatch: 'full' },
   { path: 'search', component: SearchComponent, pathMatch: 'full' },
-  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '', component: SearchComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MegaMenuModule } from 'primeng/megamenu';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { NavigationComponent } from './shared/navigation/navigation.component';
 
 import { HttpClientModule } from '@angular/common/http';
-import { CardComponent } from './shared/card/card.component';
-import { CardModule } from 'primeng/card';
-import { SharedModule } from './shared/shared.module';
-import { FormsModule } from '@angular/forms';
 import { SearchComponent } from './pages/search/search.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     SearchComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
